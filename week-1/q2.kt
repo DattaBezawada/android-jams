f4e554bfac9ecde1489e6340875837bd8fbdaff0
DattaBezawada <75771561+DattaBezawada@users.noreply.github.com>
2020-12-21 21:42:58 +0530

    Create q2.kt

---
+++ week-1/q2.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val a=12;
+    val b=15;
+    val c=9;
+    
+    if(a>b && a>c){
+        println("$a is maximum")
+    }
+    
+    else if(b>a && b>c){
+        println("$b is maximum")
+    }
+    
+    else{
+        println("$c is maximum")
+    }
+   }
