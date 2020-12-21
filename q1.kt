1552ce9fa6a785adff3b55e119745f3d9e661bc2
DattaBezawada <75771561+DattaBezawada@users.noreply.github.com>
2020-12-21 21:40:07 +0530

    Create q1.kt

---
+++ q1.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val a=5
+    if(a>0){
+        println("It is positive")
+    }
+    else if(a<0){
+        println("It is negative")
+    }
+    else{
+        println("It is 0")
+    }
+   }
