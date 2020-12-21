72a47c2fbe7c126fbed2d9d6c7a8f356a3840e77
DattaBezawada <75771561+DattaBezawada@users.noreply.github.com>
2020-12-21 21:50:34 +0530

    Create q10.kt

---
+++ week-1/q10.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+  println("Enter the height of required pyramid")
+  val n:Int=readLine()!!.toInt()
+  for(i in 1..n){
+      for(j in 1..i){
+        print("#")
+      }
+      println()
+    }
+}
