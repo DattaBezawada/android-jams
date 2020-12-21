329ea91b2ee933257e979ac151dbfbb462c30b42
DattaBezawada <75771561+DattaBezawada@users.noreply.github.com>
2020-12-21 21:57:21 +0530

    Update q3.kt

--- week-1/q3.kt
+++ week-1/q3.kt
@@ -1,9 +1,6 @@
+import kotlin.math.abs
+
 fun main(){
-    val a=55
-    if(a%5==0 && a%11==0){
-        println("$a is divisible by 5 and 11")
-    }
-    else{
-        println("$a is not divisible by 5 and 11")
-    }
+    val x = -5.4
+    print(abs(x))
 }
