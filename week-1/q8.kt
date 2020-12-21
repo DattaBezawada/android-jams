5b0e401be20ef19a6d78020e831478d887c7e9c5
DattaBezawada <75771561+DattaBezawada@users.noreply.github.com>
2020-12-21 21:49:20 +0530

    Create q8.kt

---
+++ week-1/q8.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>){
+  println("Enter any three numbers")
+  val x:Int=readLine()!!.toInt()
+  val y:Int=readLine()!!.toInt()
+  val z:Int=readLine()!!.toInt()
+  var factx:Long=1
+  var facty:Long=1
+  var factz:Long=1
+  var fact:Long
+  for(i in 1..x){
+    factx=factx*i
+  }
+  for(i in 1..y){
+    facty=facty*i
+  }
+  for(i in 1..z){
+    factz=factz*i
+  }
+
+  fact=factx+facty+factz
+  println("Sum of Factorials of given numbers $x,$y,$z are $fact")
+}
