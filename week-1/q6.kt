3eafa6e1f465973c3c20224abd5db518ffb9694f
DattaBezawada <75771561+DattaBezawada@users.noreply.github.com>
2020-12-21 21:47:47 +0530

    Create q6.kt

---
+++ week-1/q6.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+    val x=readLine()?.single()
+    when{
+    (x in 'a'..'z' || x in 'A'..'Z')->println("$x is a alphabet")
+    
+    else->println("$x is not a alphabet")
+    }   
+   }
